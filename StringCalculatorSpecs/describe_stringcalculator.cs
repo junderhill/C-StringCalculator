de1f2b3559d2eda98066f0964a5eeab9ca371008
unknown <Jason.Underhill@PC7412.udcsms.com>
2014-10-13 08:44:05 +0100

    Initial commit

---
+++ StringCalculatorSpecs/describe_stringcalculator.cs
@@ -0,0 +1,22 @@
+﻿using NSpec;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StringCalculator;
+
+namespace StringCalculatorSpecs
+{
+    class describe_stringcalculator : nspec
+    {
+        void given_a_string_of_numbers()
+        {
+            it["Takes a list of numbers in string format"] = () =>
+            {
+                StringCalc sCalc = new StringCalc();
+                sCalc.Add("1").should_be(1);
+            };
+        }
+    }
+}
