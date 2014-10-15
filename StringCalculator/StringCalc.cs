fc456cc8e48a4dacea75902ed93a7c5f1dbe1c61
Jason Underhill <jay.underhill@gmail.com>
2014-10-15 09:10:16 +0100

    Implement next few features, including custom delimiters

--- StringCalculator/StringCalc.cs
+++ StringCalculator/StringCalc.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StringCalculator
 {
     public class StringCalc
     {
+        public int Add(string numbers)
+        {
+            if (String.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+            else
+            {
+                var matches = Regex.Matches(numbers, @"^\/\/.\n");
+                string CustomDelimiter = "";
+                if(matches.Count > 0)
+                {
+                    CustomDelimiter = Regex.Replace(matches[0].Value, @"\/|\n","");
+                    numbers = Regex.Replace(numbers, @"^\/\/.\n", "");
+                }
+                return ParseStringList(numbers,CustomDelimiter).Sum();
+            }
+        }
+
+        private List<int> ParseStringList(string list, string customDelimiter)
+        {
+            List<char> delimiters = new List<char> { ',', '\n' };
+            if (!String.IsNullOrEmpty(customDelimiter))
+            {
+                delimiters.AddRange(customDelimiter.ToCharArray());
+            }
+            string[] parts = list.Split(delimiters.ToArray<char>(),StringSplitOptions.RemoveEmptyEntries);
+            List<int> toAdd = new List<int>();
+            foreach (string s in parts)
+            {
+                toAdd.Add(int.Parse(s));
+            }
+            return toAdd;
+        }
     }
 }
--- StringCalculatorSpecs/describe_stringcalculator.cs
+++ StringCalculatorSpecs/describe_stringcalculator.cs
@@ -12,11 +12,42 @@ namespace StringCalculatorSpecs
     {
         void given_a_string_of_numbers()
         {
+            StringCalc sCalc = new StringCalc();
+
             it["Takes a list of numbers in string format"] = () =>
-            {
-                StringCalc sCalc = new StringCalc();
-                sCalc.Add("1").should_be(1);
-            };
+                {
+                    sCalc.Add("1").should_be(1);
+                };
+
+            it["returns 0 if empty string"] = () =>
+                {
+                    sCalc.Add("").should_be(0);
+                };
+
+            it["returns sum of 2 numbers seperated by commas"] = () =>
+                {
+                    sCalc.Add("1,1").should_be(2);
+                };
+            it["returns sum of multiple numbers seperated by commas"] = () =>
+                {
+                    sCalc.Add("1,2,3,10,1").should_be(17);
+                };
+            it["returns sum if number seperated by new lines"] = () =>
+                {
+                    sCalc.Add("1\n2\n3\n").should_be(6);
+                };
+            it["allows a combination of delimiters"] = () =>
+                {
+                    sCalc.Add("1\n3,2").should_be(6);
+                };
+            it["supports customer delimiters"] = () =>
+                {
+                    sCalc.Add("//;\n1;2;3").should_be(6);
+                };
+            it["should throw an exception for negative numbers"] = () =>
+                {
+
+                };
         }
     }
 }
